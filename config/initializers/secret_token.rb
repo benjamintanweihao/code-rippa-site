beb651d73e8643d024c4d4f926d58883f92d3693
Benjamin Tan Wei Hao <ben@witsvale.com>
2012-03-17 00:36:00 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeRippaSite::Application.config.secret_token = '9b832f88d9db839975800150abf26e61dc658de623ffd69b15a437ca295dd22d8dca19ec6b971525d7a200a20e5ed3032d3ed083bd87c11ea73e0ab4fd605dd1'
